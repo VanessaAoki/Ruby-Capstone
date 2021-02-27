c36b8d3c91544ba9434e6c22da7fb656b3afa794
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-02-27 13:28:15 -0300

    Add some changes to the website file

--- lib/website.rb
+++ lib/website.rb
@@ -1,11 +1,11 @@
+require 'nokogiri'
+require 'open-uri'
 require_relative '../lib/tabs'
 
 class Website
-  attr_reader :url, :type, :artist, :song
-  def initialize
-    @type = type.capitalize
-    @artist = artist.capitalize
-    @song = song.capitalize
-    @url = "https://www.guitartabs.cc/search.php?tabtype=#{type}&band=#{band}&song=#{song}"
+  def initialize(website)
+    @website = siteurl(website)
+    @nokogiri = NokogiriNokogiri::HTML(URI.open(@website))
   end
+  
 end
