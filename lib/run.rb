eb0a72b589914e44713ca1af9a35d72bdaabcc34
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-02-27 18:26:17 -0300

    Add initialize method

--- lib/run.rb
+++ lib/run.rb
@@ -2,3 +2,12 @@ require 'nokogiri'
 require 'open-uri'
 require_relative '../lib/website'
 require_relative '../lib/display'
+
+class Run
+  include Display
+  attr_accessor :current_page, :active
+  def initialize
+    @current_page = nil
+    @active = true
+  end
+end
