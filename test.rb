0bb6abd7da25dcdd98f80305755e963cdb50e09c
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-03-05 02:03:08 -0300

    Fix the test file

--- test.rb
+++ test.rb
@@ -7,9 +7,12 @@ require 'webdrivers'
 require_relative './lib/display'
 
 display = Display.new
-display.select_main
 
+
+puts "Type the terms of your search: "
 input = gets.chomp.capitalize!.split(' ')
+@website = 'https://en.wikipedia.org/wiki/'
+
 
 while input.empty?
   display.invalid_article
@@ -17,22 +20,22 @@ end
 
 input.each do |term|
   if term != input.last
-    website = website + term + '+'
+    @website = @website + term + '+'
   else
-    website += term
+    @website += term
   end
 end
 
 def browser_new
   @browser = Watir::Browser.new
-  @browser.goto(website)
-  nokogiri = Nokogiri::HTML.parse(browser.html)
+  @browser.goto(@website)
+  @nokogiri = Nokogiri::HTML.parse(@browser.html)
 end
 
 browser_new
 
-title = nokogiri.css("h1#firstHeading").text
-content = nokogiri.css("div.mw-parser-output > p").text
+title = @nokogiri.css("h1#firstHeading").text
+content = @nokogiri.css("div.mw-parser-output > p").text
 
 @browser.quit
 
