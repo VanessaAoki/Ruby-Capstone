372c19de129424a3bfd1f459768c5c088424afe2
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-03-07 15:48:47 -0300

    Fix linters

--- lib/search.rb
+++ lib/search.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+# !/usr/bin/env ruby
 
 require 'nokogiri'
 require 'open-uri'
@@ -21,56 +21,58 @@ class Search
 
   def fetch_terms(input)
     @display.request_terms
-    @input = gets.chomp.split(' ')
+    @input = gets.chomp.split
     if @input.empty?
       @display.invalid_terms
       fetch_terms(input)
     end
   end
-  
-  def search_web(input)
+
+  def search_web(_input)
     @input.each do |term|
       if term == @input.last
         @website += term
       else
         @website = "#{@website}#{term}_"
       end
-    end  
+    end
   end
-  
+
   def browser_new
     @browser = Watir::Browser.new
     @browser.goto(@website)
     @nokogiri = Nokogiri::HTML.parse(@browser.html)
   end
-  
+
   def print_first
-    p1 = @nokogiri.xpath("//div//p[position()=2]").text
-    p2 = @nokogiri.xpath("//div//p[position()=3]").text
+    p1 = @nokogiri.xpath('//div//p[position()=2]').text
+    p2 = @nokogiri.xpath('//div//p[position()=3]').text
     puts p1, p2
   end
-  
+
   def keep_printing(input)
     @display.continue_article
-    input = gets.chomp.capitalize!
-    if input == 'Y'
+    @input = gets.chomp.capitalize!
+    case input
+    when 'Y'
       px = @nokogiri.css('div.mw-parser-output > p').text
       puts px
       fetch_next(input)
-    elsif input == 'N'
+    when 'N'
       fetch_next(input)
     else
       @display.invalid_continue
       keep_printing(input)
     end
   end
-  
+
   def fetch_next(input)
     @display.fetch_again
-    input = gets.chomp
-    if input == '1'
+    @input = gets.chomp
+    case input
+    when '1'
       new_search
-    elsif input == '2'
+    when '2'
       @display.goodbye
     else
       @display.invalid_again
