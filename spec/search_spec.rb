25deb31f176e819c715f40ac53e5e474711f6ca8
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-03-12 12:24:26 -0300

    Add the tests for browser_new

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -15,4 +15,14 @@ describe Search do
       expect(new_search.website).to eq("https://en.wikipedia.org/wiki/star_wars")
     end
   end
+
+  describe "#browser_new" do
+    it "returns the wikipedia title" do
+      expect(search.browser_new).to eq("Film")
+    end
+
+    it "returns the wikipedia title for second example" do
+      expect(new_search.browser_new).to eq("Star Wars")
+    end
+  end
 end
