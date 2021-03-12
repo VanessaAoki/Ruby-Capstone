8282648f111d5eaedca3e1324de50c3238d2b795
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-03-12 12:21:20 -0300

    Add tests to initialize

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,8 +1,18 @@
+require '../lib/search'
+
 describe Search do
-  let(:search) {Search.new}
-  describe "#new_search" do
-    it "returns browser quit" do
-      expect(search.new_search).to eql("Hi")
+  let(:search) { Search.new(input, var) }
+  let(:new_search) { Search.new(['star_wars'], var) }
+  let(:input) { ['movie'] }
+  let(:var) { "h1#firstHeading" }
+
+  describe "#initialize" do
+    it "returns the wikipedia url" do
+      expect(search.website).to eq("https://en.wikipedia.org/wiki/movie")
+    end
+
+    it "returns the wikipedia url with another input" do
+      expect(new_search.website).to eq("https://en.wikipedia.org/wiki/star_wars")
     end
   end
 end
